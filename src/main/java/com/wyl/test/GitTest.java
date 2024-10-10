023873c89a7ef10ee40a137439d740b7b7ad67f9
zhaofeng <18625612551@163.com>
2024-10-10 15:40:14 +0800

    hot-fix first commit

--- src/main/java/com/wyl/test/GitTest.java
+++ src/main/java/com/wyl/test/GitTest.java
@@ -9,5 +9,6 @@ public class GitTest {
         System.out.println("Hello Git!   1");
         System.out.println("Hello Git!   2");
         System.out.println("Hello Git!   3");
+        System.out.println("Hello Git!   4  -- hot-fix");
     }
 }
